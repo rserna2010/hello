e21b30fda551a724b41f9f330e675f9f0c00131e
richie serna <richie@balancedpayments.com>
2014-10-23 12:44:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '24434b7b6a29f48eba87a68b609ce97817162a8a403f1885e6ebb26dd054ff105f297872db5cddac0d841d3e3c9edf445c8ddf6373ba8bc91bc2cd9137a7cb30'
